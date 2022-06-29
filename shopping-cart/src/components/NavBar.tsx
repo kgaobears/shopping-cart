d035d4bf89d890c2658ee6add87b1a77106ff7c7
Kevin Gao <kevinyanggao@gmail.com>
2022-06-28 22:52:28 -0700

    add navbar

---
+++ shopping-cart/src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import { Navbar as NavbarBs, Container } from "react-bootstrap"
+
+export function NavBar() {
+    return (
+        <NavbarBs className="bg-white shadow-sm mb-3">
+            <Container>
+                Nav
+            </Container>
+        </NavbarBs>
+        )
+}
