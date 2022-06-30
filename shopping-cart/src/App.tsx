8b72b7f4f180dee0cbb4bfdbd42a1765f48cc570
Kevin Gao <kevinyanggao@gmail.com>
2022-06-29 19:13:38 -0700

    finish navbar

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom"
-import { Container } from "react-bootstrap"
+import { Container, Nav } from "react-bootstrap"
 import { NavBar } from "./components/NavBar"
 import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
@@ -10,7 +10,7 @@ function App() {
     return (
         <>
         <NavBar />
-        <Container>
+        <Container className="mb-4">
             <Routes>
                 <Route path="/" element={<Home />} / >
                 <Route path="/store" element={<Store />} / >
--- shopping-cart/src/components/NavBar.tsx
+++ shopping-cart/src/components/NavBar.tsx
@@ -1,10 +1,16 @@
-import { Navbar as NavbarBs, Container } from "react-bootstrap"
+import { Navbar as NavbarBs, Container, Nav } from "react-bootstrap"
+import {NavLink} from "react-router-dom"
 
 export function NavBar() {
     return (
         <NavbarBs className="bg-white shadow-sm mb-3">
             <Container>
-                Nav
+                <Nav className="me-4">
+                    <Nav.Link as={NavLink} to="/" >Home</Nav.Link>
+                    <Nav.Link as={NavLink} to="/store" >Store</Nav.Link>
+                    <Nav.Link as={NavLink} to="/about" >About</Nav.Link>
+
+                </Nav>
             </Container>
         </NavbarBs>
         )
